f282c29399ce21e1405911813032466ca47ed655
Danihmn <daniel.bezerra.mult@gmail.com>
2025-07-31 22:47:07 -0300

    Primeiro commit

---
+++ Operacoes.cs
@@ -0,0 +1,27 @@
+﻿namespace Testes_xUnit
+{
+    public class Operacoes
+    {
+        public float Adicao(float numero1, float numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public float Subtracao(float numero1, float numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        public float Multiplicacao(float numero1, float numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public float Divisao(float numero1, float numero2)
+        {
+            if (numero2 > 0)
+                return numero1 / numero2;
+            else return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Testes_xUnit
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
